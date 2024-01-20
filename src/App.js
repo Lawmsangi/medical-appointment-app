7dddcc7ddc25336ea81a96ef2de89c47f589af32
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-01-20 22:56:22 +0530

    Add router link

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Layout from './components/Layout';
+import Appointment from './components/Appointment';
+import MakeAppointment from './components/MakeAppointment';
+import Category from './components/Category';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route path="/category" element={<Layout><Category /></Layout>} />
+        <Route path="/appointment" element={<Layout><Appointment /></Layout>} />
+        <Route path="/makeappointment" element={<Layout><MakeAppointment /></Layout>} />
+
       </Routes>
     </div>
   );
