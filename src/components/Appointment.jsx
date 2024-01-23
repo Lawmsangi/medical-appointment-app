0eec32e9c89905fcf0ea226069eec53b5ce52a6d
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-01-24 01:10:35 +0530

    Update appointment when there are no appointment

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -5,28 +5,25 @@ import { removeAppointment } from '../redux/appointmentsSlice'
 function Appointment() {
   const appointments = useSelector((store) => store.appointmentsStore.appointments)
   const dispatch = useDispatch()
-  
-  if (!appointments) {
-    return (
-      <div>
-        <h1>Appointment</h1>
-        <p>No appointments</p>
-      </div>
-    )
-  }
 
   return (
     <div>
-        <h1>Appointment</h1>
-        {appointments.map((appointment) => (
+      <h1>Appointment</h1>
+      {appointments.length === 0 ? (
+        <p>No appointments</p>
+      ) : (
+        appointments.map((appointment) => (
           <div key={appointment.id}>
             <img src={appointment.image} alt="image" />
             <h3>{appointment.doctor}</h3>
             <p>{appointment.date}</p>
             <p>{appointment.time}</p>
-            <button onClick={() => dispatch(removeAppointment(appointment.id))}>Cancel</button>
+            <button onClick={() => dispatch(removeAppointment(appointment.id))}>
+              Cancel
+            </button>
           </div>
-        ))}
+        ))
+      )}
     </div>
   )
 }
